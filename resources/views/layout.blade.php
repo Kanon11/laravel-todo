9071052b148a77a67fafeb9c91cba02fd2666513
Kanon11 <kanonshet@yahoo.com>
2022-04-11 23:28:10 +0600

    packages added

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -1,12 +1,23 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    {!! HTML::style('kc_bootstrap/css/bootstrap.min.css') !!}
+    {!! HTML::style('css/kc_style.css') !!}
+
+    <title>ToDo App</title>
 </head>
+
 <body>
-    <h2>layout page</h2>
+    @include('partials.navbar')
+    <div class="container">
+        @yield('content')
+    </div>
+    @include('partials.footer')
+    {!! HTML::script('js/jquery.min.js') !!}
+    {!! HTML::script('kc_bootstrap/js/bootstrap.min.js') !!}
+    {!! HTML::script('js/kc_script.js') !!}
 </body>
+
 </html>
